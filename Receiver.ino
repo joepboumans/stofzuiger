8b355ac0f4aacfab7d28717e7c6fade53cdbc309
Ada Popa <38853753+adapopa@users.noreply.github.com>
2018-05-31 20:50:21 +0200

    Add files via upload

---
+++ Receiver.ino
@@ -0,0 +1,37 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {//Robot1 on the ramp robot 2 found the lab too
+ int message1;
+  if (Serial.available()>0 && FoundLab) {
+     message1= Serial.write(Serial.read());
+   if(message1==1){
+  while(message1==1 )
+  {stop(Robot2);}
+    }}
+
+   else 
+    if(Serial.available()>0 && !FoundLab) { //continue doing what it was doing 
+      
+    }
+    
+  
+     
+  
+}
+
+
+void send() {
+  if (FoundLab==1) //robot2 
+  { 
+    Serial.println(1); //send on the lab
+    delay(1000);
+    
+  }
+
+  
+ 
+  }
+
+
